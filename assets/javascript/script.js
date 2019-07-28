8e463a6fe9a6c7152e0ac0ec420c33a95e34b158
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-07-28 17:22:07 -0400

    put api, add css, updated js, updated html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,35 @@
-var topics = [(mathematics), (biology), (physics), (chemistry), (psychology), (philosophy), (history), (english), (organic)];
+$("button").on("click", function() {
+    // Grabbing and storing the data-animal property value from the button
+    var math = $(this).attr("data-math");
+    var biology = $(this).attr("data-biology");
+    var physics = $(this).attr("data-physics");
+    var chemistry = $(this).attr("data-chemistry");
+    var psychology = $(this).attr("data-psychology");
+    var philosophy = $(this).attr("data-philosophy");
+    var history = $(this).attr("data-history");
+    var english = $(this).attr("data-english");
+
+    // Constructing a queryURL using the animal name
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      animal + "&api_key=lP1MQYDLbYmyDynRQ563oVNiaQ4IkVFY";
+
+    // Performing an AJAX request with the queryURL
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // After data comes back from the request
+      .then(function(response) {
+        console.log(queryURL);
+
+        console.log(response);
+        // storing the data from the AJAX request in the results variable
+        var results = response.data;
+
+      });
+
+    
+      });
+
+var topics = ["mathematics", "biology", "physics", "chemistry", "psychology", "philosophy", "history", "english"] ;
+      
