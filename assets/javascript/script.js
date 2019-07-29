ca4aabe43f780491229124965f248d241a91c66d
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-07-29 14:19:23 -0400

    updated js with more scripts

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,7 @@
-$("button").on("click", function() {
+$("button").on("click", function () {
+
+    var topics = ["mathematics", "biology", "physics", "chemistry", "psychology", "philosophy", "history", "english"];
+
     // Grabbing and storing the data-animal property value from the button
     var math = $(this).attr("data-math");
     var biology = $(this).attr("data-biology");
@@ -9,27 +12,58 @@ $("button").on("click", function() {
     var history = $(this).attr("data-history");
     var english = $(this).attr("data-english");
 
-    // Constructing a queryURL using the animal name
+    // Constructing a queryURL using the subject name
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      animal + "&api_key=lP1MQYDLbYmyDynRQ563oVNiaQ4IkVFY";
+        subjects + "&api_key=lP1MQYDLbYmyDynRQ563oVNiaQ4IkVFY";
 
     // Performing an AJAX request with the queryURL
     $.ajax({
-      url: queryURL,
-      method: "GET"
+        url: queryURL,
+        method: "GET"
     })
-      // After data comes back from the request
-      .then(function(response) {
-        console.log(queryURL);
+        // After data comes back from the request
+        .then(function (response) {
+            console.log(queryURL);
 
-        console.log(response);
-        // storing the data from the AJAX request in the results variable
-        var results = response.data;
+            console.log(response);
+            // storing the data from the AJAX request in the results variable
+            var results = response.data;
+            //loops through each result item//
+            for (var a = 0; a < results.length; a++) {
 
-      });
+                // create and store div tag//
+                var subjectDiv = $("<div>");
 
-    
-      });
+                // create and store text with result item's rating//
+                var text = $("<h4>").text("Rating: " + results[a].rating);
 
-var topics = ["mathematics", "biology", "physics", "chemistry", "psychology", "philosophy", "history", "english"] ;
-      
+                //create and store img tag//
+                var image = $("<img>");
+
+                //sets src attr of img to property pulled off the result item//
+                image.attr("src", resulst[a].images.fixed_height.url);
+
+                //append text and img to the subjectDiv//
+                subjectDiv.append(text);
+                subjectDiv.append(image);
+
+                //prepends new div to index for specified div//
+                $(".gifbox").prepend(subjectDiv);
+            }
+
+            $("gifbox").on("click", function () {
+                var state = $(this).attr("data-state");
+
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+
+                };
+
+
+            });
+        });
+        }); 
