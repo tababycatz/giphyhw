b163f5fefa87ddfb6bd9abe70fcce5006236742e
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-08-01 18:40:02 -0400

    gifs now appear, fixing still and animate

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,58 +1,66 @@
-$("button").on("click", function () {
+$(document).ready(function () {
+
 
     var topics = ["cat", "mouse", "dog", "hamster"];
 
-    // Grabbing and storing the data-animal property value from the button
-    var cat = $(this).attr("data-subject");
-    var mouse = $(this).attr("data-subject");
-    var dog = $(this).attr("data-subject");
-    var fish = $(this).attr("data-subject");
+    // Function for displaying Gif data
+    function makeBtn() {
+        // Deleting the movies prior to adding new movies
+        // (this is necessary otherwise we will have repeat buttons)
+        $("#btnDump").empty();
+        // Looping through the array of movies
+        for (var i = 0; i < topics.length; i++) {
+            var b = $("<button>");
+            b.addClass("gifButton");
+            // Adding a data-attribute
+            b.attr("data-subject", topics[i]);
+            // Providing the initial button text
+            b.text(topics[i]);
+            // Adding the button to the HTML
+            $("#btnDump").append(b);
+        }
+    }
+    makeBtn()
 
-    $("button").on("click", function () {
-        // Grabbing and storing the data-animal property value from the button
-        var subject = $(this).attr("data-subject");
 
+
+    $(document).on("click", ".gifButton", function () {
+        // Grabbing and storing the data-subject property value from the button
+        var newGif = $(this).attr("data-subject");
         // Constructing a queryURL using the subject name
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "api_key=lP1MQYDLbYmyDynRQ563oVNiaQ4IkVFY";
-    });
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newGif + "&api_key=lP1MQYDLbYmyDynRQ563oVNiaQ4IkVFY";
 
-    // Performing an AJAX request with the queryURL
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-  
-        // After data comes back from the request
-        .then(function (response) {
-            console.log(queryURL);
-
+        // Performing an AJAX request with the queryURL
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
             console.log(response);
             // storing the data from the AJAX request in the results variable
             var results = response.data;
             //loops through each result item//
             for (var a = 0; a < results.length; a++) {
-
                 // create and store div tag//
                 var subjectDiv = $("<div>");
-
                 // create and store text with result item's rating//
                 var text = $("<h4>").text("Rating: " + results[a].rating);
-
                 //create and store img tag//
                 var image = $("<img>");
-
                 //sets src attr of img to property pulled off the result item//
+                image.attr("src", results[a].images.fixed_height_still.url);
                 image.attr("src", results[a].images.fixed_height.url);
+                image.attr("data-state", "still");
 
+
+                
                 //append text and img to the subjectDiv//
                 subjectDiv.append(text);
                 subjectDiv.append(image);
-
                 //prepends new div to index for specified div//
-                $("#btnDump").prepend(subjectDiv);
+                $("#gifbox").prepend(subjectDiv);
             }
 
-            $(".gifbox").on("click", function () {
+            $(".gifButton").on("click", function () {
                 var state = $(this).attr("data-state");
 
                 if (state === "still") {
@@ -64,47 +72,29 @@ $("button").on("click", function () {
 
                 };
 
-
             });
         });
+    });
 
-    // Function for displaying movie data
-    function makeBtn() {
-
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise we will have repeat buttons)
-        $(".gifbox").empty();
-
-        // Looping through the array of movies
-        for (var i = 0; i < topics.length; i++) {
-
-            var b = $("<button>");
-            b.addClass("");
-            // Adding a data-attribute
-            b.attr("data-subject", topics[i]);
-            // Providing the initial button text
-            b.text(topics[i]);
-            // Adding the button to the HTML
-            $(".gifbox").append(b);
-        }
-    }
-
-
-    $("#gif-input2").on("click", function (event) {
+    $("#gif-input").on("click", function (event) {
         // Preventing the buttons default behavior when clicked (which is submitting a form)
         event.preventDefault();
+        console.log("I was clicked")
         // This line grabs the input from the textbox
         var subbie = $("#gif-input").val().trim();
 
-        // Adding the movie from the textbox to our array
+        // Adding the new input from the textbox to our array
         topics.push(subbie);
 
-        // Calling renderButtons which handles the processing of our movie array
         makeBtn();
 
     });
 
+    // displays gifs
+    $(document).on('click', ".gif", displayGifs);
 
 
+    //call on renderbuttons fucntion
+    makeBtn();
 
-}); 
+})
